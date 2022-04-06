f4e90c26db27fd7c2fe633f449137a162f00118b
cdinezio <cdinezio@hotmail.com.ar>
2022-04-06 15:15:44 +0200

    sidebar and about me

--- Portfolio 3 - Sidebar/app.js
+++ Portfolio 3 - Sidebar/app.js
@@ -1,5 +1,10 @@
 const menuItem = document.querySelectorAll(".menu-item");
+const mainBtn = document.querySelector(".btn-primary");
+const sideBar = document.querySelector("aside");
+const main = document.querySelector("main");
+const mobileMenuBtn = document.querySelector(".mobile-menu-btn");
 
+// Le agrego el event a los links del sidebar
 menuItem.forEach(link => {
     link.addEventListener("click", activateLink)
 });
@@ -11,3 +16,22 @@ function activateLink(e) {
     const item = e.target;
     item.classList.toggle("active-link");
 }
+// END ////////////////////////////////////
+
+
+// Agrego el event de sacarle el width en ipad size al hacer click en el main section
+main.addEventListener("click",() => {
+    sideBar.classList.remove("agrandar")
+})
+// END ////////////////////////////////////
+
+mobileMenuBtn.addEventListener("click",() => {
+    sideBar.classList.toggle("agrandar")
+})
+
+
+
+
+// mainBtn.addEventListener("click",() =>{
+//     sideBar.classList.toggle("agrandar")
+// })
