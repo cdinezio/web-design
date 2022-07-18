dd4da2624b55b3087595f9602008cefd385153c1
cdinezio <cdinezio@hotmail.com.ar>
2022-07-18 11:18:03 +0200

    Intranet Upload

---
+++ Intranet Store/src/Products.js
@@ -0,0 +1,49 @@
+let Products = [{
+    id:"pc1",
+    img: "/images/pc1.jpg",
+    price: 300,
+    description: "AMD 4700S 8x3.6GHz | 16GB GDDR6 | Nvidia GTX 1650 4GB | 512GB SSD" 
+},
+{
+    id:"pc2",
+    img: "/images/pc2.jpg",
+    price: 250,
+    description: "Intel Core i5-10400F - 6x2.9GHz | 16 GB DDR4 3200 Mhz | Nvidia GeForce RTX 3050 8GB | 256GB M.2 NVMe" 
+},
+{
+    id:"pc3",
+    img: "/images/pc3.jpg",
+    price: 400,
+    description: "AMD Ryzen 5 5600X - 6x4.6GHz | 16 GB DDR4 3200 Mhz | Nvidia GTX 1650 4GB | 512GB M.2 NVMe" 
+},
+{
+    id:"pc4",
+    img: "/images/pc4.jpg",
+    price: 500,
+    description: "AMD Ryzen 7 3700X 8x4.4GHz | 16 GB DDR4 3200 Mhz | Nvidia GTX 1660Ti 6GB | 512GB M.2 NVMe + 2TB HDD" 
+},
+{
+    id:"pc5",
+    img: "/images/pc5.jpg",
+    price: 150,
+    description: "AMD Ryzen 5 5600X - 6x4.6GHz | 16 GB DDR4 3200 Mhz | Nvidia GeForce RTX 3050 8GB | 512GB M.2 NVMe" 
+},
+{
+    id:"pc6",
+    img: "/images/pc6.jpg",
+    price: 600,
+    description: "AMD Ryzen 7 3700X 8x4.4GHz | 16 GB DDR4 3200 Mhz | Nvidia GTX 1660Ti 6GB | 512GB M.2 NVMe + 1TB SSD" 
+},
+{
+    id:"pc7",
+    img: "/images/pc7.jpg",
+    price: 1200,
+    description: "AMD Ryzen 5 3600 6x4.2GHz | 16 GB DDR4 3200 Mhz | Nvidia GTX 1650 4GB | 256GB M.2 NVMe + 1TB HDD" 
+},
+{
+    id:"pc8",
+    img: "/images/pc8.jpg",
+    price: 2000,
+    description: "AMD Athlon X4 970 4x3.8 GHz | 16 GB DDR4 2666Mhz | Nvidia GTX 1650 4GB | 256GB SSD" 
+},
+]
