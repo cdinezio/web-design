da1f04f76d048067e8d09c5fd43d6d2466f32c84
cdinezio <cdinezio@hotmail.com.ar>
2022-07-19 13:06:23 +0200

    Products

--- Intranet Store/src/Products.js
+++ Intranet Store/src/Products.js
@@ -1,48 +1,48 @@
 let Products = [{
     id:"pc1",
-    img: "/images/pc1.jpg",
+    img: "src/images/pc1.png",
     price: 300,
     description: "AMD 4700S 8x3.6GHz | 16GB GDDR6 | Nvidia GTX 1650 4GB | 512GB SSD" 
 },
 {
     id:"pc2",
-    img: "/images/pc2.jpg",
+    img: "src/images/pc2.png",
     price: 250,
     description: "Intel Core i5-10400F - 6x2.9GHz | 16 GB DDR4 3200 Mhz | Nvidia GeForce RTX 3050 8GB | 256GB M.2 NVMe" 
 },
 {
     id:"pc3",
-    img: "/images/pc3.jpg",
+    img: "src/images/pc3.png",
     price: 400,
     description: "AMD Ryzen 5 5600X - 6x4.6GHz | 16 GB DDR4 3200 Mhz | Nvidia GTX 1650 4GB | 512GB M.2 NVMe" 
 },
 {
     id:"pc4",
-    img: "/images/pc4.jpg",
+    img: "src/images/pc4.png",
     price: 500,
     description: "AMD Ryzen 7 3700X 8x4.4GHz | 16 GB DDR4 3200 Mhz | Nvidia GTX 1660Ti 6GB | 512GB M.2 NVMe + 2TB HDD" 
 },
 {
     id:"pc5",
-    img: "/images/pc5.jpg",
+    img: "src/images/pc5.png",
     price: 150,
     description: "AMD Ryzen 5 5600X - 6x4.6GHz | 16 GB DDR4 3200 Mhz | Nvidia GeForce RTX 3050 8GB | 512GB M.2 NVMe" 
 },
 {
     id:"pc6",
-    img: "/images/pc6.jpg",
+    img: "src/images/pc6.png",
     price: 600,
     description: "AMD Ryzen 7 3700X 8x4.4GHz | 16 GB DDR4 3200 Mhz | Nvidia GTX 1660Ti 6GB | 512GB M.2 NVMe + 1TB SSD" 
 },
 {
     id:"pc7",
-    img: "/images/pc7.jpg",
+    img: "src/images/pc7.png",
     price: 1200,
     description: "AMD Ryzen 5 3600 6x4.2GHz | 16 GB DDR4 3200 Mhz | Nvidia GTX 1650 4GB | 256GB M.2 NVMe + 1TB HDD" 
 },
 {
     id:"pc8",
-    img: "/images/pc8.jpg",
+    img: "src/images/pc8.png",
     price: 2000,
     description: "AMD Athlon X4 970 4x3.8 GHz | 16 GB DDR4 2666Mhz | Nvidia GTX 1650 4GB | 256GB SSD" 
 },
---
+++ Intranet Store/src/app.js
@@ -0,0 +1,19 @@
+let productsContainer = document.querySelector("#products");
+
+// localStorage.setItem("computersData",JSON.stringify(Products))
+
+function generateProducts() {
+    // let productsData = JSON.parse(localStorage.getItem("computersData"))
+    productsContainer.innerHTML = Products.map((p) => {
+        return `
+        <div class="product-card">
+                <div class="product-description">${p.description}</div>
+                <img src=${p.img} alt="pc photo" srcset="">
+                <div class="product-price">$${p.price}</div>
+                <div class="cta-cart">Add to cart</div>
+            </div>
+            `
+    }).join("")
+}
+
+generateProducts();
